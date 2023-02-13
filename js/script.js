5f26e4e0440309334e8318f9845fbd3e63da413a
Rafael de Lima <rafaelftdelima@gmail.com>
2023-02-13 00:06:36 -0300

    Add active css style to header links

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const links = document.querySelector('.header-menu a');
+
+function activeLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add("active");
+    }
+}
+
+links.forEach(activeLink);
